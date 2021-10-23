a02bd2cd210bde8f3d0d6c3df002913ac5e5feac
anarcopossum <92814289+anarcopossum@users.noreply.github.com>
2021-10-22 20:48:01 -0700

    Fixed user editable values for both items and characters. Also commented

--- main.py
+++ main.py
@@ -6,27 +6,56 @@ def cnvt(j): #converts hex offset to be readable for mmap
     return(a)
 
 
+def two_hex(dest_a, dest_b, value):
+    if value <= 255: #checks if the value is less than or equal to 255
+        hexd = hex(value)
+        hexd = hex(value).replace('x', '')
+        #var = hexd.split('0x',-1)
+        if len(hexd) == 1:
+            hexd = '000'+hexd
+        elif len(hexd) == 2:
+            hexd = '00' + hexd
+
+    elif value > 255: #checks if greater than 255
+        hexd = hex(value).replace('x', '')
+        #hexd = '0'+ hexd
+        if len(hexd) == 4:
+            pass
+        elif len(hexd) == 5:
+            hexd = hexd.replace('0','',1)
+
+    splitter = []
+    for index in range(0,len(hexd),2):
+        splitter.append(hexd[index:index+2])
+    m[dest_a] = cnvt(splitter[1]) #first slot
+    m[dest_b] = cnvt(splitter[0]) #second slot
+
+
 # edits the values of characters. Note: you can use this to edit individual characters if you comment out the other ones
-def char_adj(file, Str, Dex, Int, HPa, HPb, HMa, HMb, EXa, EXb, talk):
+def char_adj(Str, Dex, Int, HPa, HPb, HMa, HMb, EXa, EXb, talk):
+    global hpv, hmv, exv, hmv
     if talk == 'c' or talk == 'C':
         cst = int(input('Strength Val (255 Max)\n'))      # Str
         cdx = int(input('Dexterity Val (255 Max)\n'))     # Dex
         cit = int(input('Intelligence Val (255 Max)\n'))  # Int
+        hpv = int(input('Health Val (999 Max)\n'))        # HP
+        hmv = int(input('Max Health Val (255 Max)\n'))    # MH
+        exv = int(input('Experience Val (255 Max)\n'))    # Int
     else:
         print('Max Values Set\n')
         cst = 99  # Str
         cdx = 99  # Dex
         cit = 99  # Int
-    m = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_WRITE)
+        hpv = 999  # HP
+        hmv = 999  # HM
+        exv = 9999  # exp
+    m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_WRITE)
     m[Str] = cst  # 63
     m[Dex] = cdx  # 63
     m[Int] = cit  # 63
-    m[HPa] = 231  # e7
-    m[HPb] = 3    # 03
-    m[HMa] = 231  # e7
-    m[HMb] = 3    # 03
-    m[EXa] = 15   # 0f
-    m[EXb] = 39   # 27
+    two_hex(HPa, HPb, hpv)
+    two_hex(HMa, HMb, hmv)
+    two_hex(EXa, EXa, exv)
     m.flush()
 
 
@@ -38,6 +67,7 @@ def item_adj(talk): #todo make the values editable for user
         mgc = int(input('How Many Magic Carpets (255 Max)\n'))  # Magic_Carpet
         gem = int(input('How Many Gems (255 Max)\n'))           # gems
         key = int(input('How Many Keys (255 Max)\n'))           # key
+        gld = int(input('How Much gold (9999 Max)\n'))          # gold
     else:
         print('\nDefault modified values set.')
         mga = 10
@@ -46,9 +76,9 @@ def item_adj(talk): #todo make the values editable for user
         mgc = 2
         gem = 100
         key = 100
+        gld = 9999
     m = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_WRITE)
-    m[cnvt('204')] = 15   # gold_a
-    m[cnvt('205')] = 39   # gold_b
+    two_hex(cnvt('204'),cnvt('205'),gld)
     m[cnvt('206')] = key  # keys
     m[cnvt('207')] = gem  # gems
     m[cnvt('20a')] = mgc  # Magic_Carpet
@@ -57,7 +87,7 @@ def item_adj(talk): #todo make the values editable for user
     m[cnvt('240')] = mga  # Magic_Axe
     m.flush()
 
-
+#two_hex(32,9)
 print('   ULTIMA V SAVE EDITOR ')
 print('\nThis tool can max the stats of all characters within the game save file creator.\n!!! Make sure this python file '
       'is in the same directory as the INIT.GAM file or SAVED.GAM in order to work properly. !!!\nyou will be able to '
@@ -80,38 +110,38 @@ char_inp = input('Would you like to modify Character Stats? (Y/N): ')
 if char_inp == 'y' or char_inp == 'Y':
     cstm = input('Would you like to (m)ax the stats? or (c)ustomize individually? (m/c): ')
     print('\n mainChar:')
-    char_adj(f, cnvt('0e'),cnvt('0f'), cnvt('10'), cnvt('12'), cnvt('13'), cnvt('14'), cnvt('15'), cnvt('16'), cnvt('17'),cstm)
+    char_adj(cnvt('0e'),cnvt('0f'), cnvt('10'), cnvt('12'), cnvt('13'), cnvt('14'), cnvt('15'), cnvt('16'), cnvt('17'),cstm)
     print('Shamino:')
-    char_adj(f, cnvt('2e'),cnvt('2f'), cnvt('30'), cnvt('32'), cnvt('33'), cnvt('34'), cnvt('35'), cnvt('36'), cnvt('37'),cstm)
+    char_adj(cnvt('2e'),cnvt('2f'), cnvt('30'), cnvt('32'), cnvt('33'), cnvt('34'), cnvt('35'), cnvt('36'), cnvt('37'),cstm)
     print('Iolo:')
-    char_adj(f, cnvt('4e'),cnvt('4f'), cnvt('50'), cnvt('52'), cnvt('53'), cnvt('54'), cnvt('55'), cnvt('56'), cnvt('57'),cstm)
+    char_adj(cnvt('4e'),cnvt('4f'), cnvt('50'), cnvt('52'), cnvt('53'), cnvt('54'), cnvt('55'), cnvt('56'), cnvt('57'),cstm)
     print('Mariah:')
-    char_adj(f, cnvt('6e'),cnvt('6f'), cnvt('70'), cnvt('72'), cnvt('73'), cnvt('74'), cnvt('75'), cnvt('76'), cnvt('77'),cstm)
+    char_adj(cnvt('6e'),cnvt('6f'), cnvt('70'), cnvt('72'), cnvt('73'), cnvt('74'), cnvt('75'), cnvt('76'), cnvt('77'),cstm)
     print('Geoffrey:')
-    char_adj(f, cnvt('8e'),cnvt('8f'), cnvt('90'), cnvt('92'), cnvt('93'), cnvt('94'), cnvt('95'), cnvt('96'), cnvt('97'),cstm)
+    char_adj(cnvt('8e'),cnvt('8f'), cnvt('90'), cnvt('92'), cnvt('93'), cnvt('94'), cnvt('95'), cnvt('96'), cnvt('97'),cstm)
     print('Jaana:')
-    char_adj(f, cnvt('Ae'),cnvt('Af'), cnvt('B0'), cnvt('B2'), cnvt('B3'), cnvt('B4'), cnvt('B5'), cnvt('B6'), cnvt('B7'),cstm)
+    char_adj(cnvt('Ae'),cnvt('Af'), cnvt('B0'), cnvt('B2'), cnvt('B3'), cnvt('B4'), cnvt('B5'), cnvt('B6'), cnvt('B7'),cstm)
     print('Julia:')
-    char_adj(f, cnvt('Ce'),cnvt('Cf'), cnvt('D0'), cnvt('D2'), cnvt('D3'), cnvt('D4'), cnvt('D5'), cnvt('D6'), cnvt('D7'),cstm)
+    char_adj(cnvt('Ce'),cnvt('Cf'), cnvt('D0'), cnvt('D2'), cnvt('D3'), cnvt('D4'), cnvt('D5'), cnvt('D6'), cnvt('D7'),cstm)
     print('Dupre:')
-    char_adj(f, cnvt('Ee'),cnvt('Ef'), cnvt('F0'), cnvt('F2'), cnvt('F3'), cnvt('F4'), cnvt('F5'), cnvt('F6'), cnvt('F7'),cstm)
+    char_adj(cnvt('Ee'),cnvt('Ef'), cnvt('F0'), cnvt('F2'), cnvt('F3'), cnvt('F4'), cnvt('F5'), cnvt('F6'), cnvt('F7'),cstm)
 
     print('Katrina:')
-    char_adj(f, cnvt('10e'),cnvt('10f'), cnvt('110'), cnvt('112'), cnvt('113'), cnvt('114'), cnvt('115'), cnvt('116'), cnvt('117'),cstm)
+    char_adj(cnvt('10e'),cnvt('10f'), cnvt('110'), cnvt('112'), cnvt('113'), cnvt('114'), cnvt('115'), cnvt('116'), cnvt('117'),cstm)
     print('Sentri:')
-    char_adj(f, cnvt('12e'),cnvt('12f'), cnvt('130'), cnvt('132'), cnvt('133'), cnvt('134'), cnvt('135'), cnvt('136'), cnvt('137'),cstm)
+    char_adj(cnvt('12e'),cnvt('12f'), cnvt('130'), cnvt('132'), cnvt('133'), cnvt('134'), cnvt('135'), cnvt('136'), cnvt('137'),cstm)
     print('Gwenno:')
-    char_adj(f, cnvt('14e'),cnvt('14f'), cnvt('150'), cnvt('152'), cnvt('153'), cnvt('154'), cnvt('155'), cnvt('156'), cnvt('157'),cstm)
+    char_adj(cnvt('14e'),cnvt('14f'), cnvt('150'), cnvt('152'), cnvt('153'), cnvt('154'), cnvt('155'), cnvt('156'), cnvt('157'),cstm)
     print('Johne:')
-    char_adj(f, cnvt('16e'),cnvt('16f'), cnvt('170'), cnvt('172'), cnvt('173'), cnvt('174'), cnvt('175'), cnvt('176'), cnvt('177'),cstm)
+    char_adj(cnvt('16e'),cnvt('16f'), cnvt('170'), cnvt('172'), cnvt('173'), cnvt('174'), cnvt('175'), cnvt('176'), cnvt('177'),cstm)
     print('Gorn:')
-    char_adj(f, cnvt('18e'),cnvt('18f'), cnvt('190'), cnvt('192'), cnvt('193'), cnvt('194'), cnvt('195'), cnvt('196'), cnvt('197'),cstm)
+    char_adj(cnvt('18e'),cnvt('18f'), cnvt('190'), cnvt('192'), cnvt('193'), cnvt('194'), cnvt('195'), cnvt('196'), cnvt('197'),cstm)
     print('Maxwell:')
-    char_adj(f, cnvt('1Ae'),cnvt('1Af'), cnvt('1B0'), cnvt('1B2'), cnvt('1B3'), cnvt('1B4'), cnvt('1B5'), cnvt('1B6'), cnvt('1B7'),cstm)
+    char_adj(cnvt('1Ae'),cnvt('1Af'), cnvt('1B0'), cnvt('1B2'), cnvt('1B3'), cnvt('1B4'), cnvt('1B5'), cnvt('1B6'), cnvt('1B7'),cstm)
     print('Toshi:')
-    char_adj(f, cnvt('1Ce'),cnvt('1Cf'), cnvt('1D0'), cnvt('1D2'), cnvt('1D3'), cnvt('1D4'), cnvt('1D5'), cnvt('1D6'), cnvt('1D7'),cstm)
+    char_adj(cnvt('1Ce'),cnvt('1Cf'), cnvt('1D0'), cnvt('1D2'), cnvt('1D3'), cnvt('1D4'), cnvt('1D5'), cnvt('1D6'), cnvt('1D7'),cstm)
     print('Saduj:')
-    char_adj(f, cnvt('1Ee'),cnvt('1Ef'), cnvt('1F0'), cnvt('1F2'), cnvt('1F3'), cnvt('1F4'), cnvt('1F5'), cnvt('1F6'), cnvt('1F7'),cstm)
+    char_adj(cnvt('1Ee'),cnvt('1Ef'), cnvt('1F0'), cnvt('1F2'), cnvt('1F3'), cnvt('1F4'), cnvt('1F5'), cnvt('1F6'), cnvt('1F7'),cstm)
     print('\nAll Characters have been modified')
 else:
     print('\nSkipped.')
@@ -136,9 +166,3 @@ if char_inp == 'exit':
     print('\n\n\n°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø')
     print('  User@Admin:~$ Sudo rm -r /*  ')
     print('°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø')
-#0e      Str
-#0f      Dex
-#10      int
-#12, 13, HP
-#14, 15, HM
-#16, 17, EX
